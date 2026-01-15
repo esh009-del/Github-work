0a76b0a62390ae4568a41817a5c04ac69e400087
Eshaan <eshaan.mohammed19@gmail.com>
2026-01-15 11:55:45 +0000

    Initial commit

---
+++ web_work/Projects/pythonexercize_date_format.py
@@ -0,0 +1,7 @@
+day=int(input("What is the day today ?"))
+month=int(input("What is the month?"))
+year=int(input("What is the year?"))
+separator = input("Enter the separator you want to use")
+print("Converted result in form of date is", end=" ")
+#tells python that we want the result printed in the same line
+print(day,separator,month,separator,year )
